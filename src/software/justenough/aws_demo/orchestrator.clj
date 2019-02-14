d9834c4f0c6e0b70bd25ad5a9b5096b0b0bc705f
Evan Niessen-Derry <eniessenderry@gmail.com>
2019-02-14 11:22:06 -0600

    Update the orchestrator based on the talk from last night
    I removed the various function and configuration vars from the
    orchestrator namespace, as they weren't really used and mostly were
    confusing during the talk.
    
    I also filled in the rest of the comment block to show case interacting
    with S3.

--- src/software/justenough/aws_demo/orchestrator.clj
+++ src/software/justenough/aws_demo/orchestrator.clj
@@ -5,75 +5,71 @@
             [software.justenough.aws-demo.iam :as iam]
             [software.justenough.aws-demo.s3 :as s3]))
 
-(def region (-> "config.edn" slurp edn/read-string :aws :region))
-(def demo-creds (-> "credentials.edn" slurp edn/read-string :aws))
-
-(def iam-creds (:iam demo-creds))
-(def s3-creds (:s3 demo-creds))
-
-(defn config
-  "Given a region as a string, and an opts map with `:access-key-id` and
-  `:secret-access-key` values defined, return a credentials-laden map for use
-  with creating a client."
-  [{:keys [region creds]}]
-  {:region               region
-   :credentials-provider (credentials/basic-credentials-provider creds)})
-
-(defn client
-  [config]
-  (aws/client config))
-
 (comment
 
-  ;; First, make a client to work with. This will pull our user level details
-  ;; from `~/.aws`
-  (def iam-client (client {:api :iam}))
+  ;; First, make an IAM client to work with. This will pull our user
+  ;; credentials from `~/.aws`
+  (def iam-client (aws/client {:api :iam}))
   (aws/validate-requests iam-client true)
 
-  ;; Second, see what ops we can call (pull up the REBL for this)
+  ;; Second, see what ops we can call
   (aws/ops iam-client)
 
-  ;; Third, show the docs for a specific operation. How does this differ from
-  ;; what the REBL can show?
+  ;; Third, show the docs for a specific operation.
   (aws/doc iam-client :ListGroups)
 
   ;; Let's make our S3 user!
-  (def s3-username "test-s3-admin")
+  (def s3-username "STEVE")
   (def demo-s3-admin (iam/create-user! iam-client s3-username))
 
-  ;; Buuuut...we don't have any access keys!
+  ;; Buuuut...we don't have any access keys, so let's make some!
   (def s3-keys (iam/create-access-key! iam-client s3-username))
-  ;; And now build our credentials
+
+  ;; And now build our credentials so we can authenticate as STEVE to S3
   (def s3-creds (credentials/basic-credentials-provider
                  {:access-key-id     (get-in s3-keys [:AccessKey :AccessKeyId])
                   :secret-access-key (get-in s3-keys [:AccessKey :SecretAccessKey])}))
-  ;; Finally creating our S3 client with our new user
-  (def s3-client (client {:api                 :s3
-                          :region               region
-                          :credentials-provider s3-creds}))
 
-  ;; Let's make a bucket!
+  ;; Finally creating our S3 client with our new user
+  (def s3-client (aws/client {:api                  :s3
+                              :region               "us-east-1"
+                              :credentials-provider s3-creds}))
+
+  ;; What buckets do we have already?
   (s3/list-buckets s3-client)
 
-  ;; Oh yeah, we need the right perms!
+  ;; Oh yeah, we need the right perms! Let's use the IAM client to see what
+  ;; groups we can put STEVE into so he has the proper permissions.
   (iam/list-group-names iam-client)
 
   ;; I guess `s3-admins` sounds right
   (iam/join-group! iam-client s3-username "s3-admins")
+
   ;; Let's see if that worked
   (iam/users-groups iam-client s3-username)
 
   ;; Primo! Let's try listing those buckets again!
   (s3/list-buckets s3-client)
 
-  ;; TODO Let's make a bucket!
+  ;; Let's make a bucket!
+  (def bucket-name "TODO YOUR FAVORITE BUCKET NAME") ; We used "chum-bucket.justenough.software"
+  (s3/create-bucket s3-client bucket-name)
 
-  ;; TODO Let's add something to it, something from the web!
+  ;; Let's add something to it, something from the web!
+  (aws/invoke s3-client {:op      :PutObject
+                         :request {:Bucket bucket-name
+                                   :Key    "crabby-patty.html"
+                                   :Body   (byte-array (map int (slurp "https://wikipedia.org")))}})
 
-  ;; TODO Ok, let's find that URL
+  ;; Let's get it back!
+  (def secret-recipe
+    (aws/invoke s3-client {:op      :GetObject
+                           :request {:Bucket bucket-name
+                                     :Key    "crabby-patty.html"}}))
 
-  ;; XXX I don't currently know of a way to generate a pre-signed URL for an S3
-  ;; object
-  ;;
-  ;; See: https://github.com/cognitect-labs/aws-api/issues/5
+  ;; Let's take a look at what came back, and then put it into a file.
+  secret-recipe
+  (spit "crabby-patty.html" (slurp (:Body secret-recipe)))
+
+  ;; And there you have it, the secret crabby patty recipe!
   )
